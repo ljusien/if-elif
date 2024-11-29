a26aa3ae9c7d0202386a0b1a99247f16171f549e
ljusien <ljusien777@gmail.com>
2024-11-29 23:24:37 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+# a = 123
+# b = 456
+# c = 789
+# if a == b and b == c or a == c:
+# print(3)
+# x = 42
+# y = 69
+# z = 42
+# if x == y or x == z:
+# print(2)
+a = 123
+b = 456
+c = 789
+if a>b and c<a:
+    print('a<b')
+elif b>a and c<b:
+    print('b<a')
+else:
+    print('0')
